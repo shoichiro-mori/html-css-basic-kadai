f14d5eccabcf74a37e8685918eb60d258bfe18a7
dai17kir51@icloud.com <dai17kir51@icloud.com>
2024-03-12 20:19:35 +0900

    課題提出

---
+++ kadai_004/src/Score_Chapter04/java/average.java
@@ -0,0 +1,11 @@
+package Score_Chapter04.java;
+
+public class average {
+
+	public static void main(String[] args) {
+		System.out.println(10 + 20 + 30 + 40 + 50 + 60 + 70 + 80 + 90 + 100);
+		System.out.println(550 / 10);
+        
+	}
+
+}
